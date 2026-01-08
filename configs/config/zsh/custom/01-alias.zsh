3117964d85613686c960d93250ad5a39eea0e6e8
Prabin Parajuli <prabinparajuli92@gmail.com>
2026-01-08 20:27:34 +0545

    Update Hyprland config and add ADB wireless alias

--- configs/config/zsh/custom/01-alias.zsh
+++ configs/config/zsh/custom/01-alias.zsh
@@ -11,7 +11,7 @@ alias reload="source ~/.config/zsh/.zshrc"
 alias tse="~/.local/bin/tmux-sessionizer"
 alias gemini="bun x gemini"
 alias ta="tmux a || tmux"
-
+alias adbwireless='adb connect $(avahi-browse -rt _adb-tls-connect._tcp -p | grep "=;" | cut -d ";" -f 8,9 | tr ";" ":")'
 # Git aliases
 alias gs='git status'
 alias ga='git add'
