6c3a3265bf1a1ffdf2600b1567b29b77073937dc
Prabin Parajuli <prabinparajuli92@gmail.com>
2026-03-28 13:05:01 +0545

    fix: form swww to awww

--- configs/config/hypr/scripts/random_wallpaper.sh
+++ configs/config/hypr/scripts/random_wallpaper.sh
@@ -4,4 +4,4 @@
 WALLPAPER=$(find ~/Pictures/wallz/ -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" -o -iname "*.gif" -o -iname "*.webp" \) -print0 | shuf -z -n 1 | xargs -0)
 
 # Set wallpaper with fast transition
-swww img "$WALLPAPER" --transition-type wipe --transition-duration 0.5 --transition-fps 60
+awww img "$WALLPAPER" --transition-type wipe --transition-duration 0.5 --transition-fps 60
