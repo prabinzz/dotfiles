95780dbcf51e4064586afe066a2df5229f893bf6
prabinzz <prabinparajuli92@gmail.com>
2026-04-26 11:46:09 +0545

    remove minimize and maximize

--- configs/config/niri/scripts/minimize_window.sh
+++
@@ -1,8 +0,0 @@
-S#!/bin/bash
-
-# Define the workspace to use for "minimized" windows
-MINIMIZE_WORKSPACE="minimized"
-
-# Move the focused column to the minimized workspace
-niri msg action move-column-to-workspace "$MINIMIZE_WORKSPACE"
-notify-send "Minimize Manager" "Window moved to $MINIMIZE_WORKSPACE workspace."
