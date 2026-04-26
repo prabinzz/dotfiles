a0a47d65a68c73fd9fbb951e10115c3fd8ef8e5a
prabinzz <prabinparajuli92@gmail.com>
2026-04-26 09:39:27 +0545

    fix: layout

--- configs/config/zsh/custom/01-alias.zsh
+++ configs/config/zsh/custom/01-alias.zsh
@@ -15,3 +15,5 @@ alias ga='git add'
 alias gc='git commit'
 alias gp='git push'
 alias gl='git log --oneline --graph --decorate'
+
+alias hotspot-on='sudo create_ap --no-virt --freq-band 2.4 wlan0 enp8s0 Prabin 12345678'
