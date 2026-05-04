f12f73f3fc3bfc01b7bc333e1722cbebe25833fe
prabinzz <prabinparajuli92@gmail.com>
2026-05-05 00:54:36 +0545

    ????

--- configs/config/zsh/custom/03-configs.zsh
+++ configs/config/zsh/custom/03-configs.zsh
@@ -6,3 +6,10 @@ eval "$(starship init zsh)"
 
 # zoxide setup
 eval "$(zoxide init zsh)"
+
+source /etc/profile
+
+## android-studio
+export ANDROID_SDK_ROOT=$HOME/Android/Sdk
+export ANDROID_HOME=$HOME/Android/Sdk
+export PATH=$ANDROID_SDK_ROOT/emulator:$ANDROID_SDK_ROOT/platform-tools:$PATH
